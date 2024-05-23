185106ef23049422cf1a7e6731a6f299404229fa
尤雨东 <root@ltpp.vip>
2024-05-23 17:50:07 +0800

    feat:update

---
+++ sh/init.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+git init;
+git remote add ltpp ssh://git@git.ltpp.vip:66/root/certbot-letencrypt-txy-ssl.git;
+git remote add github git@github.com:wmzn-ltpp/certbot-letencrypt-txy-ssl.git;

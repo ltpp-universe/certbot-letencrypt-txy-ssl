31b32e6510f3f4a8a8f83b7631b72b0bade7009c
尤雨东 <root@ltpp.vip>
2024-08-11 23:34:01 +0800

    feat:update

--- sh/init.sh
+++ sh/init.sh
@@ -2,3 +2,4 @@
 git init;
 git remote add ltpp ssh://git@git.ltpp.vip:66/root/certbot-letencrypt-txy-ssl.git;
 git remote add github git@github.com:ltpp-universe/certbot-letencrypt-txy-ssl.git;
+git remote add origin git@github.com:ltpp-universe/certbot-letencrypt-txy-ssl.git;

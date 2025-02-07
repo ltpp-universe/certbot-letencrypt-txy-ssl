409d0e9957a620f584f14c9ce6d17f04919cda5b
尤雨东 <root@ltpp.vip>
2025-02-07 23:10:05 +0800

    feat:

--- sh/init.sh
+++ sh/init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git init;
+git config core.ignorecase false;
 git remote add gitee git@gitee.com:ltpp-universe/certbot-letencrypt-txy-ssl.git;
-git remote add github git@github.com:ltpp-universe/certbot-letencrypt-txy-ssl.git;
 git remote add origin git@github.com:ltpp-universe/certbot-letencrypt-txy-ssl.git;
